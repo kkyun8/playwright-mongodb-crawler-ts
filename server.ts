4e196a52ce0e8efed10ac9b9acee4498da32ea08
kkyun8 <kim23302@icloud.com>
2022-02-10 23:20:26 +0900

    add typescript eslint, express routers

---
+++ server.ts
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import routes from "./src/routes";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 4500;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.Promise = global.Promise;
+
+const url = process.env.MONGO_URI || "url";
+const user = process.env.MONGO_USER;
+const pass = process.env.MONGO_PASSWORD;
+const dbName = process.env.MONGO_DBNAME;
+// Connect to MongoDB
+mongoose
+  .connect(url, {
+    user,
+    pass,
+    dbName,
+  })
+  .then(() => console.log("Successfully connected to mongodb"))
+  .catch((e) => console.error(e));
+
+//SAMPLE ROUTERS
+app.use(routes);
+app.listen(port, () => console.log(`Server listening on port ${port}`));
